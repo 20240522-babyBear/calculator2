1bb4309251b4f33e9e07c46256fec645f0bf7777
wdh970616 <wdh970616@naver.com>
2024-05-24 16:52:15 +0900

    숫자로 기능선택 가능하게 수정

--- src/main/java/com/babyBear/calculator2/Application.java
+++ src/main/java/com/babyBear/calculator2/Application.java
@@ -10,31 +10,42 @@ public class Application {
         System.out.println("=============================");
         System.out.println("|| S2 아기곰 계산기입니다 S2  ||");
         System.out.println("=============================");
-        System.out.println("* 원하는 기능의 이름을 입력하세요 *");
+        System.out.println("~*~~*원하는 기능을 입력하세요~*~*~");
         while (true) {
-            System.out.println("* 기능1. 더하기");
-            System.out.println("* 기능2. 빼기");
-            System.out.println("* 기능3. 곱하기");
-            System.out.println("* 기능4. 나누기");
+            System.out.println("* 기능'1'. '더하기'");
+            System.out.println("* 기능'2'. '빼기'");
+            System.out.println("* 기능'3'. '곱하기'");
+            System.out.println("* 기능'4'. '나누기'");
             System.out.print("'");
             System.out.print("종료");
             System.out.println("'를 입력하시면 계산기가 종료됩니다.");
             sign = sc.nextLine();
             if (sign.equals("종료")) {
                 break;
-            }
-            if (sign.equals("더하기") || sign.equals("빼기") || sign.equals("곱하기") || sign.equals("나누기")) {
+            } else if (sign.equals("더하기") || sign.equals("빼기") || sign.equals("곱하기") || sign.equals("나누기") || sign.equals("1") || sign.equals("2") || sign.equals("3") || sign.equals("4")) {
                 switch (sign) {
                     case "더하기":
+                        Bohyun.plus();
+                        break;
+                    case "1":
                         Bohyun.plus();
                         break;
                     case "빼기":
                         Yanghyeyeon.minus();
                         break;
+                    case "2":
+                        Yanghyeyeon.minus();
+                        break;
                     case "곱하기":
                         KangJinYoung.multiple();
                         break;
+                    case "3":
+                        KangJinYoung.multiple();
+                        break;
                     case "나누기":
+                        Seongmin.divide();
+                        break;
+                    case "4":
                         Seongmin.divide();
                         break;
                 }
